8fbf5d2b4666b64bc49bf3b93bcd039b37e09a48
nini <nikiforov.nikolai@gmail.com>
2013-07-07 10:55:11 +0200

    Solution for I.1.15. part 1&2

--- I114v1.php
+++ I114v1.php
@@ -5,24 +5,27 @@
  * Time: 1:51 PM
  */
 date_default_timezone_set('Europe/Berlin');
+header('Content-Type: text/html; charset=utf-8');
 
-//$beginOfMonth = strtotime('first day of this month');
-$beginOfMonth = strtotime('first day of previous month');
-//$daysInMonth = range(1, date("t", strtotime('July')));
-$daysInMonth = range(1, date("t", strtotime('Juny')));
-
-function emptyWeek(){
-    return array_fill(0, 7, null);
-}
-
+$beginOfMonth = strtotime('first day of this month');
+//$beginOfMonth = strtotime('first day of previous month');
+$daysInMonth = range(1, date("t", strtotime('July')));
+//$daysInMonth = range(1, date("t", strtotime('Juny')));
 
 $weeks = array();
 $week = emptyWeek();
 $indexOfDay = null;
 
+//@TODO refactor mutable mess
 foreach ($daysInMonth as $day) {
     $indexOfDay = date('N', strtotime(($day - 1) . ' day', $beginOfMonth)) - 1;
-    $week[$indexOfDay] = $day;
+    switch ($indexOfDay){
+        case 5:
+        case 6:
+            $week[$indexOfDay] = "<span style='color:red'>$day</span>"; break;
+        default:
+            $week[$indexOfDay] = $day;
+    }
     if($indexOfDay == 6) {
         $weeks[] = $week;
         $week = emptyWeek();
@@ -49,6 +52,25 @@ $renameMe = function($fn) {
 $columns = array_map($renameMe($wrapToColumn), $weeks);
 $rows = array_map($wrapToRow, $columns);
 
+echo '<p>Календарь на текущий месяц в американском формате:</p>';
 echo '<table border="1">';
 echo implode("\n", $rows);
 echo '</table>';
+
+$columns = array_map($renameMe($wrapToColumn), transpose($weeks));
+$rows = array_map($wrapToRow, $columns);
+
+echo '<p>Календарь на текущий месяц в российском формате:</p>';
+echo '<table border="1">';
+echo implode("\n", $rows);
+echo '</table>';
+
+
+function transpose($array){
+    array_unshift($array, NULL);
+    return call_user_func_array('array_map', $array);
+}
+
+function emptyWeek(){
+    return array_fill(0, 7, null);
+}
